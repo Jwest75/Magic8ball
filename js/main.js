185061ea325ac496a881eef5e6639c19aa9364a4
Jwest75 <jonathanwest303@gmail.com>
2022-12-14 18:06:04 -0800

    second commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 //create a div that holds a form with text box,a random answer generator and a submit buttion
 //create varialbe to connect to the page elements
-let answers21 = ["img/magic8ball_1.png", "img/magic8ball_2.png", "img/magic8ball_3.png", "img/magic8ball_4.png",
-   "img/magic8ball_5.png",
-   "img/magic8ball_6.png",
-   "img/magic8ball_7.png",
-   "img/magic8ball_8.png",
-   "img/magic8ball_9.png",
-   "img/magic8ball_10.png",
-   "img/magic8ball_11.png",
-   "img/magic8ball_12.png",
-   "img/magic8ball_13.png",
-   "img/magic8ball_14.png",
-   "img/magic8ball_15.png",
-   "img/magic8ball_16.png",
-   "img/magic8ball_17.png",
-   "img/magic8ball_18.png",
-   "img/magic8ball_19.png",
-   "img/magic8ball_20.png"
+let answers21 = ["./img/magic8ball_1.png", "./img/magic8ball_2.png", "./img/magic8ball_3.png", "./img/magic8ball_4.png",
+   "./img/magic8ball_5.png",
+   "./img/magic8ball_6.png",
+   "./img/magic8ball_7.png",
+   "./img/magic8ball_8.png",
+   "./img/magic8ball_9.png",
+   "./img/magic8ball_10.png",
+   "./img/magic8ball_11.png",
+   "./img/magic8ball_12.png",
+   "./img/magic8ball_13.png",
+   "./img/magic8ball_14.png",
+   "./img/magic8ball_15.png",
+   "./img/magic8ball_16.png",
+   "./img/magic8ball_17.png",
+   "./img/magic8ball_18.png",
+   "./img/magic8ball_19.png",
+   "./img/magic8ball_20.png"
 ];
 //get elements by Id
 let answers = document.getElementById("anContainer")
